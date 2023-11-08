d015361fe6e50fab139a944ccde1ff396cdf41c9
Amuseph <andrew.warzocha@gmail.com>
2023-11-08 17:03:19 -0500

    Clear Out KLARP

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 import Layout from './components/Layout/Layout'
 
 function App() {
+
   return (
     <Layout>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Project KLARP
-        </p>
-        <p>
-          Gather your friends! Its KLARPING time
-        </p>
-        <p>
-          PS: No Bully
-        </p>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          
+          <p>
+            The Project
+          </p>
+          <p>
+            Coming Soon
+          </p>
+        </header>
+      </div>
     </Layout>
   )
 }
