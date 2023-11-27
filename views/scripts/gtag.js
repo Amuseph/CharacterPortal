52bf012f7802a723d1a863012e5ae24bacd907a9
Amuseph <andrew.warzocha@gmail.com>
2023-11-26 22:37:26 -0500

    Analytics!

---
+++ views/scripts/gtag.js
@@ -0,0 +1,9 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9EHLYWJ71L"></script><script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){
+    dataLayer.push(arguments)};
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-9EHLYWJ71L');
+</script>
